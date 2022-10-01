7772dd034b06405fb90d8728503eba969a0f453c
surbat <subratj611@gmail.com>
2022-10-01 13:35:45 +0530

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container-fluid">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <img src="../assets/images/ieee.png" alt="iee image" />
+        </div>
+        <div class="col-12">
+          <h1 class="text-white">OTCON 2.0</h1>
+        </div>
+        <div class="col">
+          <p class="text-white text-center">
+            OPJU INTERNATIONAL TECHNOLOGY CONFERENCE <br />On Emerging
+            Technology for Sustainable Development (8-10 Feb, 2023)
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+<style scoped>
+.container-fluid {
+  position: relative;
+  height: 100vh;
+  width: 100%;
+  background-image: url('../assets/images/bg.jpeg');
+  background-position: center;
+  background-size: cover;
+}
+</style>
