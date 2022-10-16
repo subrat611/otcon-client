836af2ffd95e6db94c6471d8d2a84077c10a7889
Aman Vishwakarma <amankumar430206@gmail.com>
2022-10-17 00:57:00 +0530

    page wrapper component

---
+++ components/page-wrapper.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page-wrapper h-full">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.page-wrapper {
+  min-height: 90vh;
+  height: 100%;
+}
+</style>
