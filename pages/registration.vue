7f325838604893cc21da84dd55ac31bbd62a62c6
Aman Vishwakarma <amankumar430206@gmail.com>
2022-11-09 12:12:09 +0530

    registration background color change

--- pages/registration.vue
+++ pages/registration.vue
@@ -12,35 +12,31 @@
           <p class="text-dark fw-semibold">
             Account details for conference registration.
           </p>
-          <div class="card bg-success">
-            <div class="card-body text-light">
-              <div>
-                <h6 class="card-title d-inline title--color">Account Name:</h6>
-                <span>O P JINDAL UNIVERSITY</span>
-              </div>
-              <div>
-                <h6 class="card-title d-inline title--color">
-                  Account Number:
-                </h6>
-                <span>34368061214</span>
-              </div>
-              <div>
-                <h6 class="card-title d-inline title--color">Bank Name:</h6>
-                <span>State Bank of India</span>
-              </div>
-              <div>
-                <h6 class="card-title d-inline title--color">Branch Name:</h6>
-                <span>GERWANI BRANCH</span>
-              </div>
-              <div>
-                <h6 class="card-title d-inline title--color">IFSC CODE:</h6>
-                <span>SBIN0012145</span>
-              </div>
+          <div class="alert alert-info">
+            <div>
+              <h6 class="card-title d-inline title--color">Account Name:</h6>
+              <span>O P JINDAL UNIVERSITY</span>
+            </div>
+            <div>
+              <h6 class="card-title d-inline title--color">Account Number:</h6>
+              <span>34368061214</span>
+            </div>
+            <div>
+              <h6 class="card-title d-inline title--color">Bank Name:</h6>
+              <span>State Bank of India</span>
+            </div>
+            <div>
+              <h6 class="card-title d-inline title--color">Branch Name:</h6>
+              <span>GERWANI BRANCH</span>
+            </div>
+            <div>
+              <h6 class="card-title d-inline title--color">IFSC CODE:</h6>
+              <span>SBIN0012145</span>
             </div>
           </div>
           <div>
-            <p class="text-danger py-2">
-              Note: 18% GST will be applicable apart from above mentioned
+            <p class="alert alert-danger py-2">
+              *Note: 18% GST will be applicable apart from above mentioned
               registration fees.
             </p>
           </div>
