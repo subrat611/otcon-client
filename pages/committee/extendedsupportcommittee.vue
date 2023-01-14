d735e0ec7818f6071d003f8b794787fe9a90a6ef
subrat <subratj611@gmail.com>
2023-01-14 21:48:50 +0530

    extended committee add

--- pages/committee/extendedsupportcommittee.vue
+++ pages/committee/extendedsupportcommittee.vue
@@ -6,17 +6,30 @@
     <div class="container my-4 py-4">
       <div class="o-card-container">
         <div
-          v-for="({ img, name, desig, email }, i) in committee"
+          v-for="({ img, name, mobNo, membershipNo, email }, i) in committee"
           :key="i"
           class="o-card mb-3"
         >
-          <img :src="img" alt="img" class="o-card-img p-0 mb-3" />
-          <p class="m-0">{{ name }}</p>
-          <h6 class="m-0 text-danger">{{ desig }}</h6>
-          <h6 class="mt-2 text-dark">
-            {{ !email ? '' : 'Email:' }}<br />
-            <a id="email-id" :href="'mailto:' + email">{{ email }}</a>
-          </h6>
+          <div class="card-profile-container mb-2">
+            <img :src="img" alt="img" class="o-card-img p-0 mb-3" />
+            <p class="m-0">{{ name }}</p>
+          </div>
+          <div>
+            <h6 class="text-dark">
+              <span>Mobile no</span>
+              <br />
+              <span class="text-danger">{{ mobNo }}</span>
+            </h6>
+            <h6 class="text-dark">
+              <span>Membership No</span>
+              <br />
+              <span class="text-danger">{{ membershipNo }}</span>
+            </h6>
+            <h6 class="text-dark">
+              {{ !email ? '' : 'Email:' }}<br />
+              <a id="email-id" :href="'mailto:' + email">{{ email }}</a>
+            </h6>
+          </div>
         </div>
       </div>
     </div>
@@ -28,7 +41,100 @@ export default {
   name: 'ExtendedSupportCommittee',
   data() {
     return {
-      committee: [],
+      committee: [
+        {
+          img: require('assets/images/avtar.png'),
+          name: 'Vedika Agrawal',
+          mobNo: '8349910279',
+          membershipNo: '97773940',
+          email: 'vedi.ug19cse53@opju.ac.in',
+        },
+        {
+          img: require('assets/images/avtar.png'),
+          name: 'Ritika Singh',
+          mobNo: '6267425664',
+          membershipNo: '97774708',
+          email: 'riti02025@gmail.com',
+        },
+        {
+          img: require('assets/images/avtar.png'),
+          name: 'Harsh Pandey',
+          mobNo: '9630966243/8770721320',
+          altno: '8770721320',
+          membershipNo: '97775193',
+          email: 'harshrajpandey.49@gmail.com',
+        },
+        {
+          img: require('assets/images/avtar.png'),
+          name: 'V.ujjwala',
+          mobNo: '8982992201',
+          membershipNo: '97773946',
+          email: 'vungaralaujjwala2002@gmail.com',
+        },
+        {
+          img: require('assets/images/avtar.png'),
+          name: 'Utkarsh Dixit',
+          mobNo: '7735886881',
+          membershipNo: '97773964',
+          email: 'utkarshdixit2021@gmail.com',
+        },
+        {
+          img: require('assets/images/avtar.png'),
+          name: 'Monal Singh',
+          mobNo: '9131901837',
+          membershipNo: '97773934',
+          email: 'monalsingh234@gmail.com',
+        },
+        {
+          img: require('assets/images/avtar.png'),
+          name: 'Poonam Pandey',
+          mobNo: '8085950108',
+          membershipNo: '97775225',
+          email: 'poonam.shukla@jindalpower.com',
+        },
+        {
+          img: require('assets/images/avtar.png'),
+          name: 'Avaneesh Jha',
+          mobNo: '7247279229',
+          membershipNo: '96379447',
+          email: 'vipavaneesh@gmail.com',
+        },
+        {
+          img: require('assets/images/avtar.png'),
+          name: 'Shivam Kumar Jha',
+          mobNo: '8271953137',
+          membershipNo: '98266299',
+          email: 'skjha0111@gmail.com',
+        },
+        {
+          img: require('assets/images/avtar.png'),
+          name: 'Abhineet Singh',
+          mobNo: '9334257009',
+          membershipNo: '98266267',
+          email: 'abhineet9652@gmail.com',
+        },
+        {
+          img: require('assets/images/avtar.png'),
+          name: 'Shambhavi Awasthi',
+          mobNo: '8827129702',
+          membershipNo: '98227982',
+          email: 'shambhavi030902@gmail.com',
+        },
+        {
+          img: require('assets/images/avtar.png'),
+          name: 'Ambuj Raj',
+          mobNo: '9546093830',
+          membershipNo: '98228086',
+          email: 'ambhujraj6400@gmail.com',
+        },
+        {
+          img: require('assets/images/avtar.png'),
+          name: 'Suryanshu Dobai',
+          mobNo: '7008575068',
+          membershipNo: '98234195',
+          email: 'suryanshudobai@gmail.com',
+        },
+      ],
     }
   },
   head: {
@@ -47,24 +153,30 @@ export default {
   margin-top: 3rem;
   display: flex;
   flex-wrap: wrap;
-  justify-content: flex-start;
+  justify-content: space-evenly;
 }
 
-.o-card {
-  padding: 1rem;
-  width: 17rem;
-  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
-  word-wrap: break-word;
-  margin: 0 1rem;
+.card-profile-container {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  text-align: center;
+  width: 100%;
+  height: 100%;
+}
+
+.o-card {
+  padding: 1rem;
+  width: 318px;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+  display: flex;
+  margin: 0 0.2rem;
+  flex-direction: column;
+  justify-content: center;
 }
 
 .o-card-img {
-  width: 80%;
+  width: 60%;
   padding-bottom: 1rem;
   filter: grayscale(1);
 }
@@ -80,8 +192,7 @@ export default {
   .o-card {
     display: flex;
     flex-direction: column;
-    align-items: center;
-    text-align: center;
+    width: 90%;
   }
 }
 </style>
